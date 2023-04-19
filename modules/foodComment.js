8d4e5ab96abea6306305bb8eae8300b93df6d91e
Abdullah <abdullahkhrief@gmail.com>
2023-04-20 00:41:16 +0300

    The API names and images has been added

--- modules/foodComment.js
+++ modules/foodComment.js
@@ -6,15 +6,16 @@ const div = document.createElement("div")
 let mealsArray = exportedArray
 console.log(exportedArray)
 mealsArray.forEach((img, foodname, ingredient) => {
+   
     const popupC = `
 <div class="popup">
 <img class="close" onclick="this.parentNode.style.display = 'none';" src="https://static.vecteezy.com/system/resources/thumbnails/020/048/793/small/x-transparent-background-free-png.png">
-<img class="popImg" src="https://www.themealdb.com/images/media/meals/tqtywx1468317395.jpg">
-<h3 class="FoodName">${img}</h3>
+<img class="popImg" src="${img[0].strMealThumb}">
+<h3 class="FoodName">${img[0].strMeal}</h3>
 <p class="ingredients">
 The Ingredients
 </p>
-<ul>
+<ul class="ingredient">
 </ul>
 </div>
 `
