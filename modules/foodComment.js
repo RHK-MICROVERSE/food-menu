908e6fb657adecd7e5a3bb866a7f067798773b25
Abdullah <abdullahkhrief@gmail.com>
2023-04-20 01:11:10 +0300

    Added the ingredients to the popup window

--- modules/foodComment.js
+++ modules/foodComment.js
@@ -5,18 +5,38 @@ const createPop = () =>{
 const div = document.createElement("div")
 let mealsArray = exportedArray
 console.log(exportedArray)
-mealsArray.forEach((img, foodname, ingredient) => {
+mealsArray.forEach((api) => {
    
     const popupC = `
 <div class="popup">
 <img class="close" onclick="this.parentNode.style.display = 'none';" src="https://static.vecteezy.com/system/resources/thumbnails/020/048/793/small/x-transparent-background-free-png.png">
-<img class="popImg" src="${img[0].strMealThumb}">
-<h3 class="FoodName">${img[0].strMeal}</h3>
+<img class="popImg" src="${api[0].strMealThumb}">
+<h3 class="FoodName">${api[0].strMeal}</h3>
 <p class="ingredients">
 The Ingredients
 </p>
-<ul class="ingredient">
-</ul>
+<div class="ingredientC">
+${api[0].strIngredient1}
+${api[0].strIngredient2}
+${api[0].strIngredient3}
+${api[0].strIngredient4}
+${api[0].strIngredient5}
+${api[0].strIngredient6}
+${api[0].strIngredient7}
+${api[0].strIngredient8}
+${api[0].strIngredient9}
+${api[0].strIngredient10}
+${api[0].strIngredient11}
+${api[0].strIngredient12}
+${api[0].strIngredient13}
+${api[0].strIngredient14}
+${api[0].strIngredient15}
+${api[0].strIngredient16}
+${api[0].strIngredient17}
+${api[0].strIngredient18}
+${api[0].strIngredient19}
+${api[0].strIngredient20}
+</div>
 </div>
 `
 
