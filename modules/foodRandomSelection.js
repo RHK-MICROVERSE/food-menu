80b00023bfb4d163980b56de4073a7c29fd51a8e
Rassel Hassan Kadir <9rhktrading@gmail.com>
2023-04-20 16:48:25 +0600

    Update like button and Count

--- modules/foodRandomSelection.js
+++ modules/foodRandomSelection.js
@@ -35,8 +35,12 @@ const foodRandomSelection = {
           <div class="food-info">
             <p class="food-name">${meal[0].strMeal}</p>
             <p class="likeSymbol">&#10084;</p>
+            <div class="likenCount">
+              <div class="likeCount">count</div>
+              <div class="likes">likes</div>
+            </div>
           </div>
-          <p class="likes">likes</p>
+
           <div class="buttons">
             <input type="button" class="button" value="Comments">
             <input type="button" class="button" value="Reservations">
